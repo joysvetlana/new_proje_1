f8b7c2debe0e6a30da99f23c4757a5affda35bd6
Svitlana Sh <joy.svetlana@gmail.com>
2023-07-17 11:47:33 +0200

    change filename to index.html

---
+++ new_Js_lessons/main_test.js
@@ -0,0 +1,9 @@
+
+//document.write(" Hello world! javascript say hello !!");
+// console.log("Hello world! javascript say hello !!");
+// console.error("Hello world! javascript say hello !!");
+//console.warn("Hello world! javascript say hello !!");
+
+var number;
+number = 34;
+console.log( "Переменная: " + number);
